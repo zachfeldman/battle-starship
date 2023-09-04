ab1c357601eb700f58ef95cb7fa672a13120d953
Zach Feldman <zmf@frame.work>
2023-09-03 20:38:38 -0400

    Updates

---
+++ precache-manifest.54f6ada4378ea918b77f36b1bfeeab03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "939305496fd019f8c1499ba324cc491a",
+    "url": "/battle-starship/index.html"
+  },
+  {
+    "revision": "0a88ef2911e1cc58415f",
+    "url": "/battle-starship/static/css/main.176ec3c9.chunk.css"
+  },
+  {
+    "revision": "85ea1a63e3faffc9bb2d",
+    "url": "/battle-starship/static/js/2.66d98736.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/battle-starship/static/js/2.66d98736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a88ef2911e1cc58415f",
+    "url": "/battle-starship/static/js/main.4686145c.chunk.js"
+  },
+  {
+    "revision": "a4e5ce541d88ba7d8ec6",
+    "url": "/battle-starship/static/js/runtime-main.a375d35a.js"
+  }
+]);
